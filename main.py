19eb227e7a70a5231b7b31dcd7b8f22f46a748d2
Baguser7 <bagushendrawan01@gmail.com>
2024-08-07 23:53:58 +0700

    routes

--- main.py
+++ main.py
@@ -4,9 +4,5 @@ import os
 
 app = create_app()
 
-@app.route('/home')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
-
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
